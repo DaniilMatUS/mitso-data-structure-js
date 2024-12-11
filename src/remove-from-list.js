f1c5e8fad8563b281325ac9d8ff631fd2860a7a4
DaniilM <167965986+DaniilMatUS@users.noreply.github.com>
2024-12-11 03:51:19 +0300

    "feat: removeKFromList implemented"

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -18,8 +18,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   this.next = null;
  * }
  */
-
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function ListNode(x) {
+  this.value = x;
+  this.next = null;
 }
+
+module.exports = function removeKFromList(l, k) {
+  while (l !== null && l.value === k) {
+    l = l.next;
+  }
+  let current = l;
+  while (current !== null && current.next !== null) {
+    if (current.next.value === k) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return l;
+}
+
+
